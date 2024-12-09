7293b0f40def9a785d615d16527611c55a2a1c3a
benjaminmweribaya <b3njaminbaya@gmail.com>
2024-12-10 02:31:07 +0300

    feat: Use SQL to store data and retrieve it later on. Use SQLite to build relational databases on your computer. Perform CRUD operations on relational databases using SQL.

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Mr. Chocolate', 10, 'M', 'Brown', 'Calm', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Rowdy', 5, 'M', 'Black', 'Aggressive', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Tabitha', 7, 'F', 'Blonde', 'Playful', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Sergeant Brown', 9, 'M', 'Brown', 'Serious', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Melissa', 4, 'F', 'White', 'Gentle', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Grinch', 12, 'M', 'Green', 'Grumpy', 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Wendy', 6, 'F', 'Brown', 'Friendly', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES (NULL, 3, 'F', 'Grey', 'Curious', 1);
